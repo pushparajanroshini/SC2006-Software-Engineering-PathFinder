77351e6969189136dff7de80c6153b4a935066c2
yweituck <112821674+yweituck@users.noreply.github.com>
2024-04-13 22:09:19 +0800

    added PT Routing

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,14 +1,1 @@
-const myHeaders = new Headers();
-myHeaders.append("AccountKey", "Q1gFgfTJSg2kXIi4YQo+jw==");
-
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow"
-};
-
-fetch("http://datamall2.mytransport.sg/ltaodataservice/BusRoutes", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
-
+e
